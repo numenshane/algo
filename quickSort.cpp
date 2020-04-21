c94170470db95614d90deff890015cbb5244b487
池田 <numenshane@gmail.com>
2020-04-21 10:53:56 +0800

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void quickSort(unsigned int len, int arr[])
+{
+    if (len <= 1) return;
+    if (len == 2 && arr[0] > arr[1])
+    {
+        swap(arr[0], arr[1]);
+        return;
+    }
+    int val = arr[0];
+    unsigned int pos = 0;
+    int beg = 1;
+    int end = len-1;
+    
+    while(beg < end)
+    {
+        while(beg < end && val <= arr[end]) end--;
+        if(end == beg) break;
+        arr[pos] = arr[end];
+        pos = end;
+        while(end > beg && val >= arr[beg]) beg++;
+        if(end == beg) break;
+        arr[pos] = arr[beg];
+        pos = beg;
+    }
+    if(pos != 0) arr[pos] = val;
+    quickSort(pos, arr);
+    quickSort(len-pos-1, arr+pos+1);
+}
+
+void printArr(unsigned int len, int arr[])
+{
+    cout << "print arr of elements:" << len << endl;
+    for(int i=0; i<len; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+
+int main() {
+    int arr[]= {23,45,2,54,88,1,234134,2,3,4,5,6,4};
+    unsigned int len = sizeof(arr)/sizeof(*arr);
+    quickSort(len, arr);
+    printArr(len, arr);
+}
